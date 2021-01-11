922c494e4b4c6b7d6fffde56b7b68bd45eacf1c7
Haalon <haalon@yandex.com>
2021-01-11 17:48:43 +0300

    Add requirements and main for testing

---
+++ main.py
@@ -0,0 +1,20 @@
+from nltk.parse.generate import generate
+from nltk import CFG
+
+import networkx as nx
+from pnet import Pnet
+
+MAX_DEPTH = 6
+MAX_LENGTH = 14
+GRAMMAR = '''
+S -> 'S' A B | 's' C
+A -> 'A' A C | 'a'
+B -> 'b' | 'B' B A
+C -> 'C' B | 'c' 
+'''
+
+grammar = CFG.fromstring(GRAMMAR)
+
+sents = [s for s in generate(grammar, depth=MAX_DEPTH) if len(s) <= MAX_LENGTH]
+
+p = Pnet(sents)
